13f7f033242278c33662ff94b555400efd8f25d5
HaidarAhmad123 <hadiarahmad421@gmail.com>
2025-11-27 12:58:20 +0300

    feat: Add new Services2 route to App component for enhanced service offerings navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ const ServiceDetails = lazy(() =>
 const Work = lazy(() => import("./pages/Work/Work"));
 const AboutUs = lazy(() => import("./pages/about/AboutUs"));
 const Services = lazy(() => import("./pages/services/Services"));
+const Services2 = lazy(() => import("./pages/services-2/Services2"));
 const Contact = lazy(() => import("./pages/contact/Contact"));
 const News = lazy(() => import("./pages/news/News"));
 
@@ -91,6 +92,7 @@ function App() {
               <Route path="/work/:workId?" element={<Work />} />
               <Route path="/about" element={<AboutUs />} />
               <Route path="/services" element={<Services />} />
+              <Route path="/services-2" element={<Services2 />} />
               <Route path="/news" element={<News />} />
               <Route path="/contact" element={<Contact />} />
               <Route path="/influencer" element={<Influencer />} />
