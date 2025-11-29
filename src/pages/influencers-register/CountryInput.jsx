255ad1a965bd0e73783d44a8f557fa26caeaceb5
HaidarAhmad123 <hadiarahmad421@gmail.com>
2025-11-29 15:58:33 +0300

    refactor: Remove CountryInput component and update InfluencersRegister to use NumberInput for improved country selection functionality.

--- src/pages/influencers-register/CountryInput.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CountryInput = () => {
-  return (
-    <div>CountryInput</div>
-  )
-}
-
-export default CountryInput
--- src/pages/influencers-register/InfluencersRegister.jsx
+++ src/pages/influencers-register/InfluencersRegister.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import TextInput from "./TextInput";
-import NumberInput from "./CountryInput";
-import CountryInput from "./CountryInput";
+import NumberInput from "./NumberInput";
+import CountryInput from "./NumberInput";
 
 const InfluencersRegister = () => {
   return (
@@ -15,22 +15,21 @@ const InfluencersRegister = () => {
         </h1>
       </div>
       <div className="main-content px-[12px] md:px-[75px] w-full">
-      <div className="container  py-[15px] md:py-[45px] flex flex-col items-center w-full rounded-[25px] border border-[var(--foreground)]/10 bg-white">
-        <div className="title  text-center text-[var(--foreground)]  text-[20px] m:text-[24px] lg:text-[34px] xl:text-[42px] font-medium font-antonio leading-tight capitalize will-change-transform">
-          <h2>fill the registration form</h2>
-        </div>
-        <div className="main-content mt-[10px] md:mt-[30px] w-full px-[20px] md:px-[80px]">
-          <TextInput name="fullName" placeholder="Name" label={"Full Name"} />
-          <TextInput name="email" placeholder="example@gmail.com" label={"Email Address"} />
-          <div className="country-number flex">
-          <CountryInput/>
-          <TextInput name="phoneNumber" placeholder="Phone Number" label={"Phone Number"} />
+        <div className="container  py-[15px] md:py-[45px] flex flex-col items-center w-full rounded-[25px] border border-[var(--foreground)]/10 bg-white">
+          <div className="title  text-center text-[var(--foreground)]  text-[20px] m:text-[24px] lg:text-[34px] xl:text-[42px] font-medium font-antonio leading-tight capitalize will-change-transform">
+            <h2>fill the registration form</h2>
           </div>
-         
+          <div className="main-content mt-[10px] md:mt-[30px] w-full px-[20px] md:px-[80px]">
+            <TextInput name="fullName" placeholder="Name" label={"Full Name"} />
+            <TextInput
+              name="email"
+              placeholder="example@gmail.com"
+              label={"Email Address"}
+            />
+            <NumberInput />
+          </div>
         </div>
       </div>
-      </div>
-      
     </section>
   );
 };
