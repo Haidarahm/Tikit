c986c2d61ad79013af9aeaedc2eb84a96f549a45
Haidar Ahmad <haidara.ahmad@tikit.ae>
2026-01-29 09:57:55 +0300

    feat: Add error handling for dynamic module loading in main application file to improve reliability

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,20 @@ import { BrowserRouter } from "react-router-dom";
 import "./locales/i18n";
 import { I18nLanguageProvider } from "./store/I18nLanguageContext.jsx";
 import SmoothScrollProvider from "./components/SmoothScrollProvider.jsx";
+let reloaded = false;
 
+window.addEventListener('error', (e) => {
+  if (
+    !reloaded &&
+    (
+      e.message?.includes('Failed to fetch dynamically imported module') ||
+      e.message?.includes('Expected a JavaScript module script')
+    )
+  ) {
+    reloaded = true;
+    window.location.reload();
+  }
+});
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <StrictMode>
